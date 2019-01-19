d7d86f6d7ca496fee92a93ea42404609d870ce29
ringaile <ringaile@gmail.com>
2019-01-19 16:27:05 +0100

    added solution

---
+++ BlockController.js
@@ -0,0 +1,80 @@
+const SHA256 = require('crypto-js/sha256');
+const BlockClass = require('./Block.js');
+
+/**
+ * Controller Definition to encapsulate routes to work with blocks
+ */
+class BlockController {
+
+    /**
+     * Constructor to create a new BlockController, you need to initialize here all your endpoints
+     * @param {*} server 
+     */
+    constructor(server) {
+        this.server = server;
+        this.blocks = [];
+        this.initializeMockData();
+        this.getBlockByIndex();
+        this.postNewBlock();
+    }
+
+    /**
+     * Implement a GET Endpoint to retrieve a block by index, url: "/api/block/:index"
+     */
+    getBlockByIndex() {
+        this.server.route({
+            method: 'GET',
+            path: '/api/block/{index}',
+            handler: (request, h) => {
+                return JSON.parse(JSON.stringify(this.blocks[request.params.index]));        
+            }
+        });
+    }
+
+    /**
+     * Implement a POST Endpoint to add a new Block, url: "/api/block"
+     */
+    postNewBlock() {
+        this.server.route({
+            method: 'POST',
+            path: '/api/block',
+            handler: (request, h) => {
+                var newBlock = new BlockClass.Block();
+                newBlock.height = this.blocks.length;
+                // UTC timestamp
+                newBlock.time = new Date().getTime().toString().slice(0,-3);
+                // previous block hash
+                if(this.blocks.length>0){
+                    newBlock.previousBlockHash = this.blocks[this.blocks.length-1].hash;
+                }
+                // Block hash with SHA256 using newBlock and converting to a string
+                newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
+                // Adding block object to blocks
+                this.blocks.push(newBlock);
+                return 'Got a Post request!';  
+            }
+        });
+    }
+
+    /**
+     * Help method to inizialized Mock dataset, adds 10 test blocks to the blocks array
+     */
+    initializeMockData() {
+        if(this.blocks.length === 0){
+            for (let index = 0; index < 10; index++) {
+                let blockAux = new BlockClass.Block(`Test Data #${index}`);
+                blockAux.height = index;
+                blockAux.hash = SHA256(JSON.stringify(blockAux)).toString();
+                this.blocks.push(blockAux);
+            }
+        }
+    }
+
+
+}
+
+/**
+ * Exporting the BlockController class
+ * @param {*} server 
+ */
+module.exports = (server) => { return new BlockController(server);}
